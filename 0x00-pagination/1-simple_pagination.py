9407a979810b1c0a04caf3fbcb21fc1234d9d114
fuadhasen <fuadhas6634@gmail.com>
2024-10-29 18:50:12 +0530

    update simple pagination task

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -34,17 +34,16 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """method get_page"""
-            if type(page) != int or page <= 0:
-                 raise AssertionError
-            if type(page_size) != int or page_size <= 0:
-                 raise AssertionError
+        """method get_page"""
+        if isinstance(page) != int or page <= 0:
+            raise AssertionError
+        if isinstance(page_size) != int or page_size <= 0:
+            raise AssertionError
 
-            start, end = index_range(page, page_size)
-            with open(self.DATA_FILE) as f:
-                 reader = csv.reader(f)
-                 dataset = [row for row in reader]
-            start += 1
-            self.__dataset = dataset[start:end + 1]
-            return self.__dataset
-    
+        start, end = index_range(page, page_size)
+        with open(self.DATA_FILE) as f:
+            reader = csv.reader(f)
+            dataset = [row for row in reader]
+        start += 1
+        self.__dataset = dataset[start:end + 1]
+        return self.__dataset
