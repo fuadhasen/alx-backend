70708c25dd322e0591783b05d9186648233bd01f
fuadhasen <fuadhas6634@gmail.com>
2024-11-06 00:27:59 +0530

    update task 5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """scripts for simple flask App"""
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from babel import numbers, dates
 from datetime import date, datetime, time
 from flask_babel import Babel, format_date, gettext
@@ -18,6 +18,29 @@ class Config:
 app.config.from_object(Config)
 babel = Babel(app)
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """get user function"""
+    guser_id = request.args.get('login_as', type=int)
+    if guser_id and guser_id in users:
+        return guser_id
+    return None
+
+
+@app.before_request
+def before_request():
+    """befor request befor all function"""
+    user_id = get_user()
+    if user_id:
+        g.user = user_id
+
 
 @babel.localeselector
 def get_local():
@@ -31,9 +54,8 @@ def index():
     title = "Welcome to Holberton"
     head = "Hello world"
     return render_template('5-index.html', title=title,
-                           head=head)
+                           head=head, user=g.user)
 
 
 if __name__ == "__main__":
     app.run()
-
