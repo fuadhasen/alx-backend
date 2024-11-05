7055e4b8be2b3ee6504cc88efd5e42062e00e257
fuadhasen <fuadhas6634@gmail.com>
2024-11-05 23:48:00 +0530

    add task3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""scripts for simple flask App"""
+
+from flask import Flask, render_template, request
+from babel import numbers, dates
+from datetime import date, datetime, time
+from flask_babel import Babel, format_date, gettext
+
+app = Flask(__name__)
+
+
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_local():
+    """localisation function"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+home_title = gettext('home_title')
+home_header = gettext('home_header')
+
+@app.route('/')
+def index():
+    """index function"""
+    title = "Welcome to Holberton"
+    head = "Hello world"
+    return render_template('3-index.html', title=title,
+                           head=head)
+
+
+if __name__ == "__main__":
+    app.run()
