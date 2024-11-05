799c74e8e25e5b7abbd15d36ab533a765f684a02
fuadhasen <fuadhas6634@gmail.com>
2024-11-05 13:59:48 +0530

    add task 0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""scripts for simple flask App"""
+
+from flask import Flask, render_template
+from babel import numbers, dates
+from datetime import date, datetime, time
+from flask_babel import Babel, format_date, gettext
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """index function"""
+    title = 'Welcome to Holberton'
+    head = 'Hello world'
+    return render_template('index.html', title=title,
+                           head=head)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
