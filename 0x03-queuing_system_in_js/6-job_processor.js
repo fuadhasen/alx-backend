51a1723abd93d3f8d1d186fbe7c4d319beefb1d2
Fuad Hassen <133159437+fuadhasen@users.noreply.github.com>
2024-12-04 23:36:21 +0530

    Update 6-job_processor.js

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -1,11 +1,13 @@
-import { createQueue } from "kue";
+import { createQueue } from 'kue';
 
-const queue = createQueue()
+const queue = createQueue({name: 'push_notification_code'});
 
-function namedNotification(phoneNumber, message) {
-    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`)
+function sendNotification(phoneNumber, message) {
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
 }
 
 queue.process('push_notification_code', (job, done) => {
-    namedNotification(job.data.phoneNumber, job.data.message)
-})
+    const { phoneNumber, message } = job.data;
+    sendNotification(phoneNumber, message);
+    done();
+});
