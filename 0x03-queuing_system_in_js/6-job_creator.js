f7a2729f5ccdc705d02686081e28fb9f39b522ea
fuadhasen <fuadhas6634@gmail.com>
2024-12-04 11:25:00 +0530

    job create using kue

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,23 @@
+import { createQueue } from "kue"
+
+const queue = createQueue({name: 'push_notification_code'})
+
+const job = queue.create('push_notification_code', {
+    phoneNumber: 'rand',
+    message: 'randmssge'
+})
+
+
+job.on('enqueue', () => {
+    console.log(`Notification job created: ${job.id}`)
+})
+
+job.on('complete', () => {
+    console.log('Notification job completed')
+})
+
+job.on('failed attempt', () => {
+    console.log('Notification job failed')
+})
+
+job.save()
